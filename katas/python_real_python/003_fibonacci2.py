4112fea831292de7e373b246a02c16075f7f1698
gotofritz <gotofritz@users.noreply.github.com>
2023-11-12 14:25:34 +0100

    add 003_python_real_python

---
+++ katas/python_real_python/003_fibonacci2.py
@@ -0,0 +1,12 @@
+# fibonacci_multiprocessing.py
+
+import multiprocessing
+
+
+def fib(n):
+    return n if n < 2 else fib(n - 2) + fib(n - 1)
+
+
+if __name__ == "__main__":
+    for _ in range(multiprocessing.cpu_count()):
+        multiprocessing.Process(target=fib, args=(35,)).start()
